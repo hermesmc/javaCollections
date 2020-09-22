378c77a899f0f11e51412ad576b630ab983d031a
hermesmc <hermesmc@gmail.com>
2020-09-22 09:42:58 -0300

    Ordenando lista imutavel

--- src/br/com/hermestest/Curso.java
+++ src/br/com/hermestest/Curso.java
@@ -2,12 +2,13 @@ package br.com.hermestest;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Curso {
 	private String nome;
 	private String instrutor;
-	private List<Aula> aulas = new ArrayList<Aula>();
+	private List<Aula> aulas = new LinkedList<Aula>();
 	
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -32,5 +33,22 @@ public class Curso {
 		this.aulas.add(aula);
 	}
 
+//	public int getTempoTotal() {
+//		int tempoTotal = 0;
+//		for (Aula aula : aulas) {
+//			tempoTotal += aula.getTempo();			
+//		}
+//		return tempoTotal;
+//	}
+
+	public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "[Curso: " + nome + "tempo total:" + this.getTempoTotal() + "]";
+	}
 	
 }
---
+++ src/br/com/hermestest/TestaCurso2.java
@@ -0,0 +1,29 @@
+package br.com.hermestest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestaCurso2 {
+
+	public static void main(String args[]) {
+		Curso javaColecoes = new Curso("Dominando as coleções do java", "Paulo Silveira");
+		
+		javaColecoes.adiciona(new Aula("Trabalhando com colections", 21));
+		javaColecoes.adiciona(new Aula("Criando uma aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando com coleções", 22));
+		
+		List<Aula> aulasImutaveis = javaColecoes.getAulas();
+		System.out.println(aulasImutaveis);
+		
+		List<Aula> aulas = new ArrayList<>(aulasImutaveis);
+		Collections.sort(aulas);
+		System.out.println(aulas);
+	
+		System.out.println(javaColecoes.getTempoTotal());
+	}	
+	
+	
+	
+	
+}
